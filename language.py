d1e1c802907a346abdb79339a21f357dabebee0e
danijel3 <danijel@pja.edu.pl>
2023-07-19 22:39:40 +0200

    Added numbers to millions. Made other imporvements.

--- language.py
+++ language.py
@@ -1,27 +1,38 @@
-from typing import List, Set
+import re
+from typing import List, Set, Optional
+
+from spacy.tokens.token import Token
 
 from token_types import TokenGroup, TokenGroupClass
 
 punct_set = {'.', ',', '-', '!', '?', ':', '(', ')'}
 
-abbrev_set = {'ARP': 'aerpe', 'ASF': 'aesef', 'AWS': 'awues', 'BIG': 'beige', 'CAF': 'ceaef', 'CBA': 'cebea',
-              'CEIDG': 'ceidege', 'CIT': 'cit', 'COVID': 'kovid', 'ELA': 'ela', 'EOG': 'eoge', 'ETS': 'etees',
-              'GUS': 'gus', 'IPN': 'ipeen', 'KGHM': 'kagehaem', 'KIK': 'kik', 'KOWR': 'kaowuer', 'KPO': 'kapeo',
-              'KRUS': 'krus', 'KUL': 'kul', 'M': 'między', 'MSW': 'emeswu', 'MSWiA': 'emeswuia', 'NATO': 'nato',
-              'NFOŚ': 'enfoś', 'Np': ['na', 'przykład'], 'OPZZ': 'opezetzet', 'OUKiK': 'oukik', 'Ok': 'około',
-              'PAIH': 'paih', 'PARP': 'parp', 'PCR': 'peceer', 'PFR': 'peefer', 'PFRON': 'pefron', 'PGNiG': 'pegenig',
-              'PIP': 'pip', 'PIT': 'pit', 'PKB': 'pekabe', 'PKP': 'pekape', 'PL': 'peel', 'PO': 'peo', 'POLSA': 'polsa',
-              'POZ': 'poze', 'PPS': 'pepees', 'PR': 'peer', 'PRL': 'peerel', 'PSL': 'peesel', 'PUP': 'pup',
-              'PiS': 'pis', 'RDS': 'erdees', 'REGON': 'regon', 'RP': 'erpe', 'SMS': 'esemes', 'TVP': 'tefaupe',
-              'UE': 'ue', 'UOKiK': 'uokik', 'VAT': 'vat', 'WE': 'wue', 'WUP': 'wup', 'ZFA': 'zetefa', 'ZFRON': 'zefron',
-              'ZUS': 'zus', 'art': 'atrykuł', 'br': ['bieżącego', 'roku'], 'dr': 'doktor', 'godz': 'godzina',
-              'im': 'imienia', 'in': 'innymi', 'm': 'między', 'np': ['na', 'przykład'], 'ok': 'około',
-              'prof': 'profesor', 'r': 'rok', 'tj': ['to', 'jest'], 'tyg': 'tygodnia', 'tys': 'tysięcy',
-              'tzn': ['to', 'znaczy'], 'tzw': ['tak', 'zwany'], 'ub': 'ubiegłego', 'ust': 'ustęp',
-              'ww': ['wyżej', 'wymieniony'], 'św': 'święty', 'w': 'wiek'}
+# abbreviations requiring period after them
+abbrev_period_set = {'art': ['atrykuł'], 'br': ['bieżącego', 'roku'],
+                     'dr': ['doktor'], 'godz': ['godzina'], 'im': ['imienia'], 'in': ['innymi'], 'm': ['między'],
+                     'np': ['na', 'przykład'], 'ok': ['około'], 'prof': ['profesor'], 'r': ['rok'],
+                     'tj': ['to', 'jest'],
+                     'tyg': ['tygodnia'], 'tys': ['tysięcy'], 'tzn': ['to', 'znaczy'], 'tzw': ['tak', 'zwany'],
+                     'ub': ['ubiegłego'], 'ust': ['ustęp'], 'ww': ['wyżej', 'wymieniony'], 'św': ['święty'],
+                     'w': ['wiek']}
+
+abbrev_set = {'ARP': ['aerpe'], 'ASF': ['aesef'], 'AWS': ['awues'], 'BIG': ['beige'], 'CAF': ['ceaef'],
+              'CBA': ['cebea'], 'CEIDG': ['ceidege'], 'CIT': ['cit'], 'COVID': ['kovid'], 'ELA': ['ela'],
+              'EOG': ['eoge'], 'ETS': ['etees'], 'GUS': ['gus'], 'IPN': ['ipeen'], 'KGHM': ['kagehaem'], 'KIK': ['kik'],
+              'KOWR': ['kaowuer'], 'KPO': ['kapeo'], 'KRUS': ['krus'], 'KUL': ['kul'], 'M': ['między'],
+              'MSW': ['emeswu'], 'MSWiA': ['emeswuia'], 'NATO': ['nato'], 'NFOŚ': ['enfoś'], 'Np': ['na', 'przykład'],
+              'OPZZ': ['opezetzet'], 'OUKiK': ['oukik'], 'Ok': ['około'], 'PAIH': ['paih'], 'PARP': ['parp'],
+              'PCR': ['peceer'], 'PFR': ['peefer'], 'PFRON': ['pefron'], 'PGNiG': ['pegenig'], 'PIP': ['pip'],
+              'PIT': ['pit'], 'PKB': ['pekabe'], 'PKP': ['pekape'], 'PL': ['peel'], 'PO': ['peo'], 'POLSA': ['polsa'],
+              'POZ': ['poze'], 'PPS': ['pepees'], 'PR': ['peer'], 'PRL': ['peerel'], 'PSL': ['peesel'], 'PUP': ['pup'],
+              'PiS': ['pis'], 'RDS': ['erdees'], 'REGON': ['regon'], 'RP': ['erpe'], 'SMS': ['esemes'],
+              'TVP': ['tefaupe'], 'UE': ['ue'], 'UOKiK': ['uokik'], 'VAT': ['vat'], 'WE': ['wue'], 'WUP': ['wup'],
+              'ZFA': ['zetefa'], 'ZFRON': ['zefron'], 'ZUS': ['zus'],
+              'PR24': ['polskie', 'radio', 'dwadzieścia', 'cztery'], 'TVP3': ['tefaupe', 'trzy'], 'S&F': ['esenef']}
+
 romannumeral_set = {'I', 'V', 'X', 'L', 'C', 'M'}
 
-symbol_set = {'%': 'procent', '+': 'plus', '#': ['hasz', 'tag']}
+symbol_set = {'%': ['procent'], '+': ['plus'], '#': ['hasz', 'tag']}
 
 
 def get_abbrev_candidates(lines: List[List[TokenGroup]]) -> Set[str]:
@@ -52,56 +63,134 @@ def get_abbrev_candidates(lines: List[List[TokenGroup]]) -> Set[str]:
     return ret
 
 
-d1_10 = {1: 'jeden', 2: 'dwa', 3: 'trzy', 4: 'cztery', 5: 'pięć', 6: 'sześć', 7: 'siedem', 8: 'osiem', 9: 'dziewięć'}
 u1k = 'tysiąc'
 u2_4k = 'tysiące'
 u5_9k = 'tysięcy'
+u1m = 'milion'
+u2_4m = 'miliony'
+u5_9m = 'milionów'
 h1_9 = {1: 'sto', 2: 'dwieście', 3: 'trzysta', 4: 'czterysta', 5: 'pięćset', 6: 'sześćset', 7: 'siedemset',
         8: 'osiemset', 9: 'dziewięćset'}
 t2_9 = {2: 'dwadzieścia', 3: 'trzydzieści', 4: 'czterdzieści', 5: 'pięćdziesiąt', 6: 'sześćdziesiąt',
         7: 'siedemdziesiąt', 8: 'osiemdziesiąt', 9: 'dziwiędziesiąt'}
-digits = {0: 'zero', 1: 'jeden', 2: 'dwa', 3: 'trzy', 4: 'cztery', 5: 'pięć', 6: 'sześć', 7: 'siedem', 8: 'osiem',
-          9: 'dziewięć', 10: 'dziesięć', 11: 'jedenaście', 12: 'dwanaście', 13: 'trzynaście', 14: 'czternaście',
-          15: 'piętnaście', 16: 'szesnaście', 17: 'siedemnaście', 18: 'osiemnaście', 19: 'dziewiętnaście'}
+d0_19 = {0: 'zero', 1: 'jeden', 2: 'dwa', 3: 'trzy', 4: 'cztery', 5: 'pięć', 6: 'sześć', 7: 'siedem', 8: 'osiem',
+         9: 'dziewięć', 10: 'dziesięć', 11: 'jedenaście', 12: 'dwanaście', 13: 'trzynaście', 14: 'czternaście',
+         15: 'piętnaście', 16: 'szesnaście', 17: 'siedemnaście', 18: 'osiemnaście', 19: 'dziewiętnaście'}
+
+dig_cleanup = re.compile(r'\D')
 
 
-def integer_to_text(dig: str) -> List[str]:
+def int_to_str(num: int) -> List[str]:
     ret = []
-    num = int(dig)
     if num == 0:
-        return [digits[num]]
+        return [d0_19[num]]
     if num > 999:
-        d = num // 1000
+        t = num // 1000
         num = num % 1000
-        if d == 1:
+        if t >= 1000:
+            m = t // 1000
+            t = t % 1000
+            if m % 10 == 1:
+                if m > 1:
+                    ret.extend(int_to_str(m))
+                ret.append(u1m)
+            elif 2 <= m % 10 <= 4:
+                ret.extend(int_to_str(m))
+                ret.append(u2_4m)
+            else:
+                ret.extend(int_to_str(m))
+                ret.append(u5_9m)
+            if t == 0:
+                return ret
+        if t % 10 == 1:
+            if t > 1:
+                ret.extend(int_to_str(t))
             ret.append(u1k)
-        elif 2 <= d <= 4:
-            ret.append(d1_10[d])
+        elif 2 <= t % 10 <= 4:
+            ret.extend(int_to_str(t))
             ret.append(u2_4k)
-        elif 5 <= d <= 9:
-            ret.append(d1_10[d])
+        else:
+            ret.extend(int_to_str(t))
             ret.append(u5_9k)
+        if num == 0:
+            return ret
     if num > 99:
-        d = num // 100
+        t = num // 100
         num = num % 100
-        ret.append(h1_9[d])
+        ret.append(h1_9[t])
     if num > 19:
-        d = num // 10
+        t = num // 10
         num = num % 10
-        ret.append(t2_9[d])
+        ret.append(t2_9[t])
     if num > 0:
-        ret.append(digits[num])
+        ret.append(d0_19[num])
     return ret
 
 
-roman_map = {'I': '1', 'II': '2', 'III': '3', 'IV': '4', 'V': '5', 'VI': '6', 'VII': '7', 'VIII': '8', 'IX': '9',
-             'X': '10', 'XX': '20', 'XIX': '19', 'C': '100'}
+def intstr_to_text(dig: str) -> Optional[List[str]]:
+    dig = dig_cleanup.sub('', dig)
+    if not dig:
+        return None
+    return int_to_str(int(dig))
+
+
+roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 
 def roman_to_text(num: str) -> List[str]:
-    if num in roman_map:
-        return integer_to_text(roman_map[num])
+    val = 0
+    i = 0
+    while i < len(num):
+        x = roman_val[num[i]]
+        if i + 1 < len(num):
+            xn = roman_val[num[i + 1]]
+            if x >= xn:
+                val += x
+                i += 1
+            else:
+                val += xn - x
+                i += 2
+        else:
+            val += x
+            i += 1
+    return intstr_to_text(str(val))
 
-    # DEBUG
-    # print('ROM: ' + num)
-    return ['??']
+
+def number_to_text(num: Token) -> Optional[List[str]]:
+    form = num.morph.get('NumForm')
+    if form and form[0] == 'Word':
+        return [num.lower_]
+    if num.is_digit:
+        return intstr_to_text(num.text)
+    else:
+        if not num.is_alpha:
+            if ',' in num.text:
+                tok = num.text.split(',')
+                if len(tok) == 2:
+                    ret = intstr_to_text(tok[0])
+                    ret.append('przecinek')
+                    ret.extend(intstr_to_text(tok[1]))
+                    return ret
+            if '/' in num.text:
+                tok = num.text.split('/')
+                if len(tok) == 2:
+                    ret = intstr_to_text(tok[0])
+                    ret.append('przez')
+                    ret.extend(intstr_to_text(tok[1]))
+                    return ret
+        return [num.lower_]
+
+
+def parse_time(time: str) -> Optional[List[str]]:
+    if 4 <= len(time) <= 5 and ':' in time and time[0].isdigit() and time[3].isdigit():
+        ret = []
+        if len(time) == 4 and time[1] == ':' and time[2].isdigit():
+            ret.extend(intstr_to_text(time[0:1]))
+            ret.extend(intstr_to_text(time[2:4]))
+            return ret
+        elif len(time) == 5 and time[2] == ':' and time[1].isdigit() and time[4].isdigit():
+            ret.extend(intstr_to_text(time[0:2]))
+            ret.extend(intstr_to_text(time[3:5]))
+            return ret
+    else:
+        return None
